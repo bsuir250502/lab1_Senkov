648423c92a5e7c43928eb2692abe194add2e08ba
AlekseiSenkov <senkkov-leha@mail.ru>
2013-03-28 12:48:39 +0300

    improved version2

--- lib1.h
+++ lib1.h
@@ -5,15 +5,15 @@
 char *input_valid_data()    /* function for checking input infomation (srting). */
 {   
     char *input_text; 
-	input_text = (char *)malloc(sizeof(char));                        
-    do {
-        __fpurge(stdin);
+    input_text = (char *)malloc(sizeof(char));                        
+    do {    
+        __fpurge(stdin) 
         fgets(input_text, 30, stdin);
         if (input_text[0] == '\n' || input_text[0] == ' ') {
             puts("Invalid input, try again.");
             continue;
-		}
-		else
+        }
+        else
             break;
     } while (1);
     return input_text;
@@ -25,5 +25,5 @@ void students_quantity(int *number_of_students)
     while (!scanf("%d", &*number_of_students) || *number_of_students < 1) {   
         __fpurge(stdin);
         puts("Invalid input, try again.");
-	}
+    }
 }
